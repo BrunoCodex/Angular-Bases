8b2c34153a660ff81d45a1e28c3e9f2bff8b530b
Bruno Sanchez Oviedo <bruno_libra_11@hotmail.com>
2023-01-03 12:35:22 -0500

    auto-generar componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'bases';
+  // title = 'Bases de Angular.js';
+  // numero: number = 10;
+  // base: number = 5;
+
+  // sumar(){
+  //   this.numero += 1;
+  // }
+
+  // restar(){
+  //   this.numero -= 1;
+  // }
+
+  // acumular(valor : number){
+  //   this.numero += valor;
+  // }
 }
