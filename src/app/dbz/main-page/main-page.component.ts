5f9647cb2bf9385eb1b2627bf2dd60311e7ce633
Bruno Sanchez Oviedo <bruno_libra_11@hotmail.com>
2023-01-03 23:47:16 -0500

    Mostrar listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,12 +12,29 @@ interface Personaje{
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 14000
+    },
+    {
+      nombre: 'Vegueta',
+      poder: 9500
+    }
+  ]
+
   nuevo: Personaje = {
     nombre: '',
     poder: 0
   }
 
   agregar(){
-    console.log("Evento de formulario "+this.nuevo.nombre+" - "+this.nuevo.poder);
+    // console.log("Evento de formulario "+this.nuevo.nombre+" - "+this.nuevo.poder);
+    if(this.nuevo.nombre.trim().length === 0){ return; }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre : '',
+      poder : 0
+    }
   }
 }
