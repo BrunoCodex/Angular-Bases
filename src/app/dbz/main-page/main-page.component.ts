5986861c33b0a564a714e44f1b45d0268f6b5916
Bruno Sanchez Oviedo <bruno_libra_11@hotmail.com>
2023-01-03 23:32:56 -0500

    Two way data binding angular

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
   agregar(){
-    console.log("Evento de formulario");
+    console.log("Evento de formulario "+this.nuevo.nombre+" - "+this.nuevo.poder);
   }
 }
